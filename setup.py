3bc7906159b1242b7d0d2705feea16004138bb67
Titusz Pan <titusz.pan@gmail.com>
2018-10-02 15:31:30 +0200

    pin to older ccxt as latest one fails with dependency errors of eth-utils

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     packages=find_packages(),
-    install_requires=['requests', 'requests_cache', 'ccxt'],
+    install_requires=['requests', 'requests_cache', 'ccxt==1.14.141'],
     package_data={'cryptop': ['config.ini']},
     entry_points = {
         'console_scripts': ['cryptop = cryptop.cryptop:main'],
